1c4d3fe1ba8e2a5095007e91a94763dd2e03f2a0
Davy Blavette <davy.blavette@akairnet.com>
2021-12-15 17:26:33 +0100

    first

---
+++ dataset/fer2013/train.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv('./fer2013.csv')
+
+width, height = 48, 48
+
+datapoints = data['pixels'].tolist()
+
+# getting features for training
+X = []
+for xseq in datapoints:
+    xx = [int(xp) for xp in xseq.split(' ')]
+    xx = np.asarray(xx).reshape(width, height)
+    X.append(xx.astype('float32'))
+
+X = np.asarray(X)
+X = np.expand_dims(X, -1)
+
+# getting labels for training
+y = pd.get_dummies(data['emotion']).to_numpy()
+
+# storing them using numpy
+np.save('fdataX', X)
+np.save('flabels', y)
+
+print("Preprocessing Done")
+print("Number of Features: " + str(len(X[0])))
+print("Number of Labels: " + str(len(y[0])))
+print("Number of examples in dataset:" + str(len(X)))
+print("X,y stored in fdataX.npy and flabels.npy respectively")
