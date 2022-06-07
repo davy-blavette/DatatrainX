e4a9255406dc62113c40727dc7a79bf98b468982
Davy Blavette <davy.blavette@akairnet.com>
2022-06-07 15:44:17 +0200

    v 1.0.2 - licence

--- src/datatrainx/layout/Footer.svelte
+++ src/datatrainx/layout/Footer.svelte
@@ -55,12 +55,12 @@
         <span class="logo-text">
             <Logo color={"ffffff"} />
         </span>
-        <span class="version">Version 1.0.1</span>
+        <span class="version">Version 1.0.2</span>
         <span class="version">{userId}</span>
         <span class="link" on:click={() => animateScroll.scrollTo({element: "#header", onDone: (element, offset) => {layoutStore.setLayout("opendata")}})}>OpenData</span>
         <p>
             By <a href="https://fr.linkedin.com/in/davy-blavette-8876b952">Davy Blavette</a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+            <a href="https://github.com/davy-blavette/DatatrainX/blob/main/LICENSE">MIT</a>. The website content
             is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
         <div class="icon" title="Open-source code"><a target="_blank" href="https://github.com/davy-blavette/DatatrainX"><i class="fa-brands fa-github"></i></a></div>
